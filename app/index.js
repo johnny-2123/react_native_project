2d7511f307119f2f9f6bb8ad81bbc55f075a16cc
edgar <johnnyavila2021@u.northwestern.edu>
2023-08-15 00:46:21 -0400

    initialize directory structure

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES, FONTS } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
