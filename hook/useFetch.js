db7a71cb4569280538bd1003fb27c6765aaf37ec
edgar <johnnyavila2021@u.northwestern.edu>
2023-08-17 00:52:25 -0400

    update fetch request to correctly handle response from jsearch API

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -5,17 +5,16 @@ const rapidApiKey = process.env.RAPID_API_KEY;
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
+  console.log("data", data);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const url =
+    "https://jsearch.p.rapidapi.com/search?query=Python%20developer%20in%20Texas%2C%20USA&page=1&num_pages=1";
   const options = {
     method: "GET",
-    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-    params: {
-      ...query,
-    },
     headers: {
-      "X-RapidAPI-Key": rapidApiKey,
+      "X-RapidAPI-Key": "abc8c9758bmshd1c86a7081cb67dp13033ajsnf8a7f320b6b8",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
   };
@@ -24,11 +23,14 @@ const useFetch = (endpoint, query) => {
     setIsLoading(true);
 
     try {
-      const response = await axios.request(options);
-      setData(response.data);
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log("result", result);
+      setData(result);
       setIsLoading(false);
     } catch (error) {
       setError(error);
+      console.log("error", error);
       alert("there is an error", error);
     } finally {
       setIsLoading(false);
